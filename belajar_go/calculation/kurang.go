beb3587b6b455613950e1b8b2666e748d780b567
useryesa9@gmail.com <useryesa9@gmail.com>
2025-01-26 08:42:40 +0700

    variable

---
+++ belajar_go/calculation/kurang.go
@@ -0,0 +1,5 @@
+package calculation
+
+func Kurang(kesatu int, kedua int) int {
+	return kesatu - kedua
+}
---
+++ belajar_go/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+  //go bahasa static type data
+  var name string = "muhamad yesa"
+  var name1 string
+  name1 = "yesa aja"
+
+  name2 := "muhamad"
+  name2 = "progammer"
+
+
+  fmt.Println(name)
+  fmt.Println(name1)
+
+  fmt.Println(name2)
+}
