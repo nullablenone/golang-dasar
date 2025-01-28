03b724edda3f58a8678753de69ceff71fa330585
useryesa9@gmail.com <useryesa9@gmail.com>
2025-01-28 22:52:04 +0700

    for

--- belajar_go/main.go
+++ belajar_go/main.go
@@ -1,16 +1,9 @@
 package main
 
-func main() {
-	angka := 4
+import "fmt"
 
-	switch angka {
-	case 1:
-		println("Satu")
-	case 2:
-		println("Dua")
-	case 3:
-		println("Tiga")
-	default:
-		println("NULL")
+func main() {
+	for i := 1; 1 <= 100; i++ {
+		fmt.Println("saya sedang belajar go", i)
 	}
 }
