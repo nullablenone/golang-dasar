77f36cf4afbccda469c768d5f67be5d5947d448c
useryesa9@gmail.com <useryesa9@gmail.com>
2025-01-28 23:06:52 +0700

    while versi go

--- belajar_go/main.go
+++ belajar_go/main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; 1 <= 100; i++ {
-		fmt.Println("saya sedang belajar go", i)
+	iterasi := 1
+
+	for iterasi <= 100{
+		fmt.Println("menjalankan", iterasi)
+		iterasi++
 	}
+	
 }
