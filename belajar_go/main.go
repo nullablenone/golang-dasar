0e6f44cb31b886c1f98ac10620496363f2fa5329
useryesa9@gmail.com <useryesa9@gmail.com>
2025-01-29 08:05:42 +0700

    inisialisasi array

--- belajar_go/main.go
+++ belajar_go/main.go
@@ -4,14 +4,7 @@ import "fmt"
 
 func main() {
 
-	// sama seperti variable, array juga punya nilai default
-	var language [5]string
-
-	language[0] = "php"
-	language[1] = "jsht"
-	language[2] = "java"
-	language[3] = "c"
-	language[4] = "ruby"
+	language := [5]string{"c++", "js", "html", "css", "php"}
 
 	fmt.Println(language)
 }
