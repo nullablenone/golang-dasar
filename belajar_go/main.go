085320d367e0a17617ae6f90137dfbf061543a4e
useryesa9@gmail.com <useryesa9@gmail.com>
2025-01-29 08:04:07 +0700

    deklarasi array

--- belajar_go/main.go
+++ belajar_go/main.go
@@ -1,35 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	//mencetak cika index bilangan genap
-	//mencetak jika iterasi nya huruf vokal
 
-	teks := "Golang the bast languange"
+	// sama seperti variable, array juga punya nilai default
+	var language [5]string
 
-	for index, iterasi := range teks {
+	language[0] = "php"
+	language[1] = "jsht"
+	language[2] = "java"
+	language[3] = "c"
+	language[4] = "ruby"
 
-		if index%2 == 0 {
-			
-			switch iterasi {
-			case 'a', 'i', 'u', 'e', 'o':
-				fmt.Println("index :", index, "car :", string(iterasi))
-
-				// case 'a':
-				// 	fmt.Println("index :", index, "car :", string(iterasi))
-				// case 'i':
-				// 	fmt.Println("index :", index, "car :", string(iterasi))
-				// case 'u':
-				// 	fmt.Println("index :", index, "car :", string(iterasi))
-				// case 'e':
-				// 	fmt.Println("index :", index, "car :", string(iterasi))
-				// case 'o':
-				// 	fmt.Println("index :", index, "car :", string(iterasi))
-			}
-
-		}
-	}
+	fmt.Println(language)
 }
